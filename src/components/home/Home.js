3e63cdbb977d32fff36bbc1115ef9efbe4a1b2fb
felipezintzun <felipezintzun@gmail.com>
2022-04-13 20:22:41 -0500

    created homepage with icon placeholders for future use

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,10 +1,50 @@
 import React from "react";
 import "./home.css";
+import Plant from "../../img/plant-image-home.png";
+import { GiGardeningShears } from "react-icons/gi";
+import { RiPlantFill } from "react-icons/ri";
+import { IoFlowerSharp } from "react-icons/io5";
+
+
 
 function Home () {
     return (
         <div className="home">
-          This is our Home!
+          <div className="h-left">
+            <div className="h-left wrapper">
+              <h2 className="h-intro">Let's Grow</h2>
+            
+            <div className="h-description">
+            Flower Power is here to create a plant-based community for you and your plants
+            to flourish. We want you to share your photos and stories of your plant friends. 
+            The tips and tricks you have learned along the way will help everyone grow. You
+            can connect with the community to: sell, trade, or simply socialize. 
+            Inspired by plants, lets grow together.
+            </div>
+
+            <div className="icon-wrapper">
+              <div className="icon-cont">
+                <a className='i-icon' href="" target="_blank" rel="noreferrer"><GiGardeningShears size='50'/></a>
+              </div>    
+
+              <div className="icon-cont">
+                <a className='i-icon' href="" target="_blank" rel="noreferrer"> <RiPlantFill size='50'/></a>
+              </div>
+
+              <div className="icon-cont">
+                <a className='i-icon' href="" target="_blank" rel="noreferrer"> <IoFlowerSharp size='50'/></a>
+              </div>
+              
+            </div>
+            </div>
+          </div>
+
+          <div className="h-right">
+                <div className="h-card bg"></div>
+                    <div className="h-card">
+                    <img src={Plant} alt="" className="i-image" />
+                    </div>
+            </div>
 
         </div>
 
