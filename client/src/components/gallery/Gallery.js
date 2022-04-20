1131144aa5545da2b8b092d156a6c44d82ea1ebb
felipezintzun <felipezintzun@gmail.com>
2022-04-19 21:03:40 -0500

    added button for flower talk

--- client/src/components/gallery/Gallery.js
+++ client/src/components/gallery/Gallery.js
@@ -1,7 +1,10 @@
 import React from "react";
 import "./gallery.css";
+import {Link} from  'react-router-dom';
 
 function Gallery ({img,link,name}) {
+
+
   return (
 
       <div className="gallery">
@@ -15,9 +18,16 @@ function Gallery ({img,link,name}) {
             </a>
             
             <div className="card-text">
+
               <h4 className="flower-name">{name}</h4>
               <p className="flower-description">This is a post about this flower</p>
+
             </div>
+            
+              <Link to={"/thoughts"} className="fThought-btn">Flower Talk</Link>
+
+            
+            
 
           </article>
 
