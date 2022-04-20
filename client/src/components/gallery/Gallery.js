4137d15501a75006080d4177ebe198b3cbbf49f5
felipezintzun <felipezintzun@gmail.com>
2022-04-20 18:32:35 -0500

    fixed the flower image problem

--- client/src/components/gallery/Gallery.js
+++ client/src/components/gallery/Gallery.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./gallery.css";
 import {Link} from  'react-router-dom';
 
-function Gallery ({img,link,name}) {
+function Gallery ({img,link,name, description}) {
 
 
   return (
@@ -13,18 +13,18 @@ function Gallery ({img,link,name}) {
 
           <article className="flower-card">
 
-            <a href={link} target="_blank" rel="noreferrer">
+            <a href={"/thoughts"} target="_blank" rel="noreferrer">
                 <img src={img} alt="" className="gallery-img"/>
             </a>
             
             <div className="card-text">
 
               <h4 className="flower-name">{name}</h4>
-              <p className="flower-description">This is a post about this flower</p>
+              <p className="flower-description">{description}</p>
+              <Link to={"/thoughts"} className="fThought-btn">Flower Talk</Link>
 
             </div>
             
-              <Link to={"/thoughts"} className="fThought-btn">Flower Talk</Link>
 
             
             
